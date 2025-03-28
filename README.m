f3dfd142907c75d02a63a23f27d67dbb47b0e170
jordan te ka <jordanteka256@gmail.com>
2025-03-29 00:28:50 +0300

    Update README.m

--- README.m
+++ README.m
@@ -1,9 +1,9 @@
 
 - 👋 Hi, I’m @jordanteka
 - 👀 I’m interested in programing
-- 🌱 I’m currently learning python language
+- 🌱 I’m an expert python language.
 - 💞️ I’m looking to collaborate on projects
-- 📫 How to reach me just react to my respiratories
+- 📫 How to reach me just react to my respositories
 
 <!---
 jordanteka/jordanteka is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
